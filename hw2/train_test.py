8bd4bda5faa898685fc01493b190ad264de77fac
Taejun Kim <i2r.jun@gmail.com>
2019-04-25 15:49:19 +0900

    a

--- hw2/train_test.py
+++ hw2/train_test.py
@@ -12,93 +12,99 @@ import models
 from hparams import hparams
 from densenet import DenseNet
 
+
 # Wrapper class to run PyTorch model
 class Runner(object):
-    def __init__(self, hparams):
-        # self.model = models.Baseline(hparams)
-        self.model = DenseNet(drop_rate=0.0, num_classes=len(hparams.genres))
-        self.criterion = torch.nn.CrossEntropyLoss()
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=hparams.learning_rate, momentum=hparams.momentum)
-        self.scheduler = ReduceLROnPlateau(self.optimizer, mode='min', factor=hparams.factor, patience=hparams.patience, verbose=True)
-        self.learning_rate = hparams.learning_rate
-        self.stopping_rate = hparams.stopping_rate
-        self.device = torch.device("cpu")
+  def __init__(self, hparams):
+    # self.model = models.Baseline(hparams)
+    self.model = DenseNet(drop_rate=0.0, num_classes=len(hparams.genres))
+    self.criterion = torch.nn.CrossEntropyLoss()
+    self.optimizer = torch.optim.SGD(self.model.parameters(), lr=hparams.learning_rate,
+                                     momentum=hparams.momentum, nesterov=True)
+    self.scheduler = ReduceLROnPlateau(self.optimizer, mode='min', factor=hparams.factor, patience=hparams.patience,
+                                       verbose=True)
+    self.learning_rate = hparams.learning_rate
+    self.stopping_rate = hparams.stopping_rate
+    self.device = torch.device("cpu")
 
-        if hparams.device > 0:
-            torch.cuda.set_device(hparams.device - 1)
-            self.model.cuda(hparams.device - 1)
-            self.criterion.cuda(hparams.device - 1)
-            self.device = torch.device("cuda:" + str(hparams.device - 1))
+    if hparams.device > 0:
+      torch.cuda.set_device(hparams.device - 1)
+      self.model.cuda(hparams.device - 1)
+      self.criterion.cuda(hparams.device - 1)
+      self.device = torch.device("cuda:" + str(hparams.device - 1))
 
-    # Accuracy function works like loss function in PyTorch
-    def accuracy(self, source, target):
-        source = source.max(1)[1].long().cpu()
-        target = target.cpu()
-        correct = (source == target).sum().item()
+  # Accuracy function works like loss function in PyTorch
+  def accuracy(self, source, target):
+    source = source.max(1)[1].long().cpu()
+    target = target.cpu()
+    correct = (source == target).sum().item()
 
-        return correct/float(source.size(0))
+    return correct / float(source.size(0))
 
-    # Running model for train, test and validation. mode: 'train' for training, 'eval' for validation and test
-    def run(self, dataloader, mode='train'):
-        self.model.train() if mode is 'train' else self.model.eval()
+  # Running model for train, test and validation. mode: 'train' for training, 'eval' for validation and test
+  def run(self, dataloader, mode='train'):
+    self.model.train() if mode is 'train' else self.model.eval()
 
-        epoch_loss = 0
-        epoch_acc = 0
-        for batch, (x, y) in enumerate(dataloader):
-            x = x.to(self.device)
-            y = y.to(self.device)
+    epoch_loss = 0
+    epoch_acc = 0
+    for batch, (x, y) in enumerate(dataloader):
+      x = x.to(self.device)
+      y = y.to(self.device)
 
-            prediction = self.model(x)
-            loss = self.criterion(prediction, y)
-            acc = self.accuracy(prediction, y)
+      prediction = self.model(x)
+      loss = self.criterion(prediction, y)
+      acc = self.accuracy(prediction, y)
 
-            if mode is 'train':
-                loss.backward()
-                self.optimizer.step()
-                self.optimizer.zero_grad()
+      if mode is 'train':
+        loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
 
-            epoch_loss += prediction.size(0)*loss.item()
-            epoch_acc += prediction.size(0)*acc
+      epoch_loss += prediction.size(0) * loss.item()
+      epoch_acc += prediction.size(0) * acc
 
-        epoch_loss = epoch_loss/len(dataloader.dataset)
-        epoch_acc = epoch_acc/len(dataloader.dataset)
+    epoch_loss = epoch_loss / len(dataloader.dataset)
+    epoch_acc = epoch_acc / len(dataloader.dataset)
 
-        return epoch_loss, epoch_acc
+    return epoch_loss, epoch_acc
 
-    # Early stopping function for given validation loss
-    def early_stop(self, loss, epoch):
-        self.scheduler.step(loss, epoch)
-        self.learning_rate = self.optimizer.param_groups[0]['lr']
-        stop = self.learning_rate < self.stopping_rate
+  # Early stopping function for given validation loss
+  def early_stop(self, loss, epoch):
+    self.scheduler.step(loss, epoch)
+    self.learning_rate = self.optimizer.param_groups[0]['lr']
+    stop = self.learning_rate < self.stopping_rate
 
-        return stop
+    return stop
+
 
 def device_name(device):
-    if device == 0:
-        device_name = 'CPU'
-    else:
-        device_name = 'GPU:' + str(device - 1)
+  if device == 0:
+    device_name = 'CPU'
+  else:
+    device_name = 'GPU:' + str(device - 1)
 
-    return device_name
+  return device_name
+
 
 def main():
-    train_loader, valid_loader, test_loader = data_manager.get_dataloader(hparams)
-    runner = Runner(hparams)
+  train_loader, valid_loader, test_loader = data_manager.get_dataloader(hparams)
+  runner = Runner(hparams)
 
-    print('Training on ' + device_name(hparams.device))
-    for epoch in range(hparams.num_epochs):
-        train_loss, train_acc = runner.run(train_loader, 'train')
-        valid_loss, valid_acc = runner.run(valid_loader, 'eval')
+  print('Training on ' + device_name(hparams.device))
+  for epoch in range(hparams.num_epochs):
+    train_loss, train_acc = runner.run(train_loader, 'train')
+    valid_loss, valid_acc = runner.run(valid_loader, 'eval')
 
-        print("[Epoch %d/%d] [Train Loss: %.4f] [Train Acc: %.4f] [Valid Loss: %.4f] [Valid Acc: %.4f]" %
-              (epoch + 1, hparams.num_epochs, train_loss, train_acc, valid_loss, valid_acc))
+    print("[Epoch %d/%d] [Train Loss: %.4f] [Train Acc: %.4f] [Valid Loss: %.4f] [Valid Acc: %.4f]" %
+          (epoch + 1, hparams.num_epochs, train_loss, train_acc, valid_loss, valid_acc))
 
-        if runner.early_stop(valid_loss, epoch + 1):
-            break
+    if runner.early_stop(valid_loss, epoch + 1):
+      break
 
-    test_loss, test_acc = runner.run(test_loader, 'eval')
-    print("Training Finished")
-    print("Test Accuracy: %.2f%%" % (100*test_acc))
+  test_loss, test_acc = runner.run(test_loader, 'eval')
+  print("Training Finished")
+  print("Test Accuracy: %.2f%%" % (100 * test_acc))
+
 
 if __name__ == '__main__':
-    main()
+  main()
