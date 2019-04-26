75f93b6c89228c640133da248d4b6a250b57993b
Taejun Kim <i2r.jun@gmail.com>
2019-04-27 00:11:59 +0900

    Pretty print

--- hw2/train_test.py
+++ hw2/train_test.py
@@ -101,12 +101,10 @@ def main():
   for epoch in range(hparams.num_epochs):
     train_loss, train_acc = runner.run(train_loader, 'train')
     valid_loss, valid_acc = runner.run(valid_loader, 'eval')
-    
     test_loss, test_acc = runner.run(test_loader, 'test')
-    print("Test Accuracy: %.2f%%" % (100 * test_acc * 10))
-
-    print("[Epoch %d/%d] [Train Loss: %.4f] [Train Acc: %.4f] [Valid Loss: %.4f] [Valid Acc: %.4f]" %
-          (epoch + 1, hparams.num_epochs, train_loss, train_acc, valid_loss, valid_acc))
+    
+    print("[Epoch %d/%d] [Train Loss: %.4f] [Train Acc: %.4f] [Valid Loss: %.4f] [Valid Acc: %.4f] [Test Acc: %.4f]" %
+          (epoch + 1, hparams.num_epochs, train_loss, train_acc, valid_loss, valid_acc, test_acc))
 
     if runner.early_stop(valid_loss, epoch + 1):
       break
